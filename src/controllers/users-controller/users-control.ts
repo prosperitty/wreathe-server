47d6a45286f31767b0a5002e20f43d67737d08ec
Alex Marmolejo <alex97marmolej@gmail.com>
2023-08-14 02:57:04 -0400

    removed test logic

--- src/controllers/users-controller/users-control.ts
+++ src/controllers/users-controller/users-control.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcryptjs'
 const prisma = new PrismaClient()
 
 /* GET users listing. */
@@ -8,40 +7,4 @@ export const usersGet = async (req: Request, res: Response) => {
   const allUsers = await prisma.wreathe_user.findMany()
   console.log(allUsers)
   res.json(allUsers)
-}
-
-export const usersPost = async (req: Request, res: Response) => {
-  const encryptedPassword = await bcrypt.hash('johndoe', 10)
-  await prisma.wreathe_user.create({
-    data: {
-      first_name: 'john',
-      last_name: 'doe',
-      email: 'johndoe@gmail.com',
-      username: 'johndoe',
-      user_password: encryptedPassword,
-      thread: {
-        create: {
-          content: 'hello world',
-        },
-      },
-    },
-  })
-
-  const allUsers = await prisma.wreathe_user.findMany({
-    include: {
-      thread: true,
-    },
-  })
-
-  console.log(allUsers)
-  res.json(allUsers)
-}
-
-export const usersUpdate = async (req: Request, res: Response) => {
-  const thread = await prisma.thread.update({
-    where: { thread_uid: ' ' },
-    data: { content: 'hello world updated' },
-  })
-  console.log(thread)
-  res.json(thread)
 }
