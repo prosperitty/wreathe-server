65674769f7c480294b69c4ea15606eb5ab2d0980
Alex Marmolejo <alex97marmolej@gmail.com>
2023-08-14 02:51:18 -0400

    init messages controller

--- src/controllers/users-controller/messages-control.ts
+++ src/controllers/users-controller/messages-control.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export const messagesGet = (req: Request, res: Response) => {
   res.send('list of the users messages page')
